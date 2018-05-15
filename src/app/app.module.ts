fdc5027643946cb1cc520be898a52ec366a4268b
Claudya Novery <claudyanovery@gmail.com>
2018-05-15 11:52:06 +0900

    Creating test page with material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,6 +31,25 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatListModule,
+    MatProgressBarModule,
+	MatSlideToggleModule,
+	MatInputModule,
+	MatFormFieldModule,
+	BrowserAnimationsModule,
+	MatRadioModule,
+	MatSelectModule,
+	MatCardModule,
+	MatExpansionModule,
+	MatTabsModule,
+	MatTreeModule,
+	MatIconModule,
+	MatBadgeModule,
+	MatBottomSheetModule,
+	MatDialogModule,
+	MatPaginatorModule,
+	MatSortModule,
+	MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
