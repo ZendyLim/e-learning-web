f50de2da8a9113b1923843fe02a736dcc1c84ff0
harfendy chen <tenjack96@gmail.com>
2018-06-24 00:48:49 +0900

    insert all graph images

--- src/app/corporate/sidebar/corporate-sidebar.component.ts
+++ src/app/corporate/sidebar/corporate-sidebar.component.ts
@@ -10,7 +10,7 @@ export class CorporateSidebarComponent implements OnInit {
   items: {name, link}[] = [
     {name: 'Home', link: '/corporate'},
     {name: 'User', link: '/corporate/user'},
-    {name: 'Corporate', link: '/corporate/corporate'},
+    {name: 'Corporate', link: '/corporate/analytic'},
     {name: 'Logout', link: '/'},
   ];
 
